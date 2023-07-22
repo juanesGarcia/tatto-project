34cab4988118f038e8d897ecfaf9e477b7a42f52
juanes <67403119+juanesGarcia@users.noreply.github.com>
2023-07-22 00:28:37 -0500

    poniendo el https://tattopro.vercel.app/ en vez de localhost esta vez de manera correcta

--- src/api/auth.js
+++ src/api/auth.js
@@ -6,7 +6,7 @@ export async function onUpdate(updateData) {
     console.log(updateData.user)
     try {
       const response = await axios.put(
-        `https://tattopro.vercel.app/user/${updateData.id}`,
+        `https://tatto-backend.onrender.com/user/${updateData.id}`,
         updateData.user
       );
   
@@ -17,42 +17,42 @@ export async function onUpdate(updateData) {
   }
 export async function onRegistration(registrationData){
     return await axios.post(
-        'https://tattopro.vercel.app/register',
+        'https://tatto-backend.onrender.com/register',
         registrationData
     );
 }
 
 export async function onLogin(loginData){
     return await axios.post(
-        'https://tattopro.vercel.app/login',
+        'https://tatto-backend.onrender.com/login',
         loginData
     );
 }
 
 export async function onLogout(){
     return await axios.get(
-        'https://tattopro.vercel.app/logout'
+        'https://tatto-backend.onrender.com/logout'
     );
 }
 
 export async function fechProtectedInfo(){
     return await axios.get(
-        'https://tattopro.vercel.app/protected'
+        'https://tatto-backend.onrender.com/protected'
     );
 }
 export async function getUsers(){
     return await axios.get(
-        'https://tattopro.vercel.app/user'
+        'https://tatto-backend.onrender.com/user'
     );
 }
 export async function getUser(id){
     return await axios.get(
-        `https://tattopro.vercel.app/user/${id}`
+        `https://tatto-backend.onrender.com/user/${id}`
     );
 }
 
 export async function onDelete(id){
     return await axios.delete(
-        `https://tattopro.vercel.app/user/${id}`
+        `https://tatto-backend.onrender.com/user/${id}`
     );
 }
