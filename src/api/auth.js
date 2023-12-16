6bf7e630be611b966712e2de6a3ab8315d553ed5
juanes <67403119+juanesGarcia@users.noreply.github.com>
2023-12-16 16:43:40 -0500

    preba de pull request

--- src/api/auth.js
+++ src/api/auth.js
@@ -6,7 +6,7 @@ export async function onUpdate(updateData) {
     console.log(updateData.user)
     try {
       const response = await axios.put(
-        `https://tattopro.com/user/${updateData.id}`,
+        `https://tatto-backend.onrender.com/user/${updateData.id}`,
         updateData.user
       );
   
@@ -17,42 +17,42 @@ export async function onUpdate(updateData) {
   }
 export async function onRegistration(registrationData){
     return await axios.post(
-        'https://tattopro.com/register',
+        'https://tatto-backend.onrender.com/register',
         registrationData
     );
 }
 
 export async function onLogin(loginData){
     return await axios.post(
-        'https://tattopro.com/login',
+        'https://tatto-backend.onrender.com/login',
         loginData
     );
 }
 
 export async function onLogout(){
     return await axios.get(
-        'https://tattopro.com/logout'
+        'https://tatto-backend.onrender.com/logout'
     );
 }
 
 export async function fechProtectedInfo(){
     return await axios.get(
-        'https://tattopro.com/protected'
+        'https://tatto-backend.onrender.com/protected'
     );
 }
 export async function getUsers(){
     return await axios.get(
-        'https://tattopro.com/user'
+        'https://tatto-backend.onrender.com/user'
     );
 }
 export async function getUser(id){
     return await axios.get(
-        `https://tattopro.com/user/${id}`
+        `https://tatto-backend.onrender.com/user/${id}`
     );
 }
 
 export async function onDelete(id){
     return await axios.delete(
-        `https://tattopro.com/user/${id}`
+        `https://tatto-backend.onrender.com/user/${id}`
     );
 }
