326fb6d1465cf489e7548e5ec7146d343abc2297
juanes <67403119+juanesGarcia@users.noreply.github.com>
2023-12-16 17:03:20 -0500

    preba de main a vercel

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,27 +1,47 @@
 import React from 'react';
-import "../Styles/Footer.css"
+import '../Styles/Footer.css';
 import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
 import InstagramIcon from '@mui/icons-material/Instagram';
-import TwitterIcon from '@mui/icons-material/Twitter';
+import MiImagen from './icons8-threads-48.png';
 
 export const Footer = () => {
-    return (
-        <div className="containerFooter">
-            <div className="footer">
-                <div className='contact'>
-                    <h1>Contact us</h1>
-                </div>
-                <div className="footer-contact">
-                    <p>Correo: juanesgym2018@gmail.com</p>
-                    <p>Teléfono: +57 3186699925 </p>
-                </div>
-                <div className='container-social'>
-                    <a className="container-icons" href="https://web.facebook.com/juanesteban.cubillosgarcia.7/"> <FacebookRoundedIcon className='icons' sx={{ marginLeft: "5% !important" }}></FacebookRoundedIcon></a>
-                    <a className="container-icons" href="https://www.instagram.com/juanestebancubillos/"> <InstagramIcon className='icons' sx={{ marginLeft: "5% !important" }}></InstagramIcon></a>
-                    <a className="container-icons" href="https://www.instagram.com/juanestebancubillos/"><TwitterIcon  className='icons' sx={{ marginLeft: "5% !important" }}></TwitterIcon></a>
-                    
-                </div>
-            </div>
+  return (
+    <div className="containerFooter">
+      <div className="footer">
+        <div className="contact">
+          <h1>Contact us</h1>
         </div>
-    )
-}
+        <div className="footer-contact">
+          <p>Correo: juanesgym2018@gmail.com</p>
+          <p>Teléfono: +57 3186699925 </p>
+        </div>
+        <div className="container-social">
+          <a
+            className="container-icons"
+            href="https://web.facebook.com/profile.php?id=61554505598366"
+            target="_blank" // Abre el enlace en una nueva pestaña o ventana
+            title="Facebook"
+          >
+            <FacebookRoundedIcon className="icons" sx={{ marginLeft: '5% !important' }} />
+          </a>
+          <a
+            className="container-icons"
+            href="https://www.instagram.com/tattoproart/"
+            target="_blank" // Abre el enlace en una nueva pestaña o ventana
+            title="Instagram"
+          >
+            <InstagramIcon className="icons" sx={{ marginLeft: '5% !important' }} />
+          </a>
+          <a
+            className="container-icons"
+            href="https://www.threads.net/@tattoproart"
+            target="_blank" // Abre el enlace en una nueva pestaña o ventana
+            title="Threads"
+          >
+            <img src={MiImagen} alt="Descripción de la imagen" className="iconsT" sx={{ marginLeft: '5% !important' }} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
