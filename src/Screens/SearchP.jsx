263dbe49c3591515f3c43abd4c242a950042d096
juanes <67403119+juanesGarcia@users.noreply.github.com>
2024-06-16 16:42:17 -0500

    acomodando texto de searchp

--- src/Screens/SearchP.jsx
+++ src/Screens/SearchP.jsx
@@ -188,7 +188,7 @@ export const SearchP = () => {
            
           </div>
         ))}
-          <div className='titlemap'>mira cabron colombia falcao los  en la cuidad de {cityUserM} (busca por cuidades)</div>
+          <div className='titlemap'>mira los tatuadores mas cercanos a ti en la cuidad de {cityUserM} (busca por cuidades)</div>
 
         <Mapa users={users} setcityUserM={setcityUserM}></Mapa>
     </div>
